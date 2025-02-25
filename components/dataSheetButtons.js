4591a41db7635fa7756b981b024dfa57e5f12be5
David Hurrell <dhurrell@hotmail.co.uk>
2025-02-25 17:25:57 +0000

    Add comments

--- components/dataSheetButtons.js
+++ components/dataSheetButtons.js
@@ -4,28 +4,37 @@ import SubmitButton from "./submitButton";
 import { storage } from "../firebaseConfig";
 import { ref, getDownloadURL } from "firebase/storage";
 import { Linking } from "react-native";
+
+/*Finction to open PDF in browser, retrieves the download URL from Firebase Storage
+and opens the URL in the browser fileName: the name of the file in 
+Firebase Storage to open */
+
 const openPdfInBrowser = async (fileName) => {
   try {
-    const pdfRef = ref(storage, fileName);
-    const url = await getDownloadURL(pdfRef);
+    const pdfRef = ref(storage, fileName); // Create a reference to the PDF file in Firebase Storage
+    const url = await getDownloadURL(pdfRef); // Get the download URL for the PDF
     console.log("PDF Download URL:", url);
 
-    await Linking.openURL(url); // Alternative method
+    await Linking.openURL(url); // Open the PDF in the browser
   } catch (error) {
     console.error("Error fetching PDF:", error);
   }
 };
 
+/* Component to display buttons for viewing TDS and SDS, uses selectedPlaster prop
+to determine which plaster's TDS and SDS to display */
+
 export default function DataSheetButtons({ selectedPlaster }) {
   console.log(selectedPlaster);
+  console.log(selectedPlaster.tdsFileName);
   return (
     <View style={styles.container}>
       <SubmitButton
-        title="View SDS"
-        onPress={() => openPdfInBrowser("MultiFinishTDS.pdf")}
+        title="View TDS"
+        onPress={() => openPdfInBrowser(selectedPlaster?.tdsFileName)}
       />
       <SubmitButton
-        title="View TDS"
+        title="View SDS"
         onPress={() => openPdfInBrowser("anotherFile.pdf")}
       />
     </View>
