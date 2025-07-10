40a2db9314ac4fc472b2fd4a77f2a72498e8ec8f
David Hurrell <dhurrell@hotmail.co.uk>
2025-07-10 18:09:42 +0100

    Added search search modal for testing currently commented out as search has been moved to nav. bar - Ready for refactoring!

--- components/appMain.jsx
+++ components/appMain.jsx
@@ -1,5 +1,5 @@
 // appMain.js
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AppContext } from "./appMainContext";
 import { View, Text, ScrollView, StyleSheet } from "react-native";
 import PlasterTypeSwitch from "./plasterTypeSwitches";
@@ -7,8 +7,14 @@ import PlasterDropdown from "./dropdownPicker";
 import InputDisplayArea from "./inputDisplayArea";
 import OutputDisplayArea from "./outputDisplayArea";
 import HorizontalRule from "./horizontalRule";
+import SearchPlasterComponent from "./searchPlasters";
 import { StatusBar } from "expo-status-bar";
 import DataSheetButtons from "./dataSheetButtons";
+import { Modal, Button } from "react-native";
+import PlasterSearchModal from "./plasterSearchModal";
+
+/* Main component for the app, contains all the other components
+ */
 const AppMain = () => {
   const {
     lengthInput,
@@ -30,54 +36,62 @@ const AppMain = () => {
     contingencyInput,
     setContingencyInput,
   } = useContext(AppContext);
-
   return (
-    <ScrollView contentContainerStyle={styles.scrollViewContent}>
-      <View style={styles.container}>
-        <Text style={styles.title}>PLASTER CALCULATOR</Text>
-        <HorizontalRule />
-        <PlasterTypeSwitch
-          InternalisEnabled={InternalisEnabled}
-          ExternalisEnabled={ExternalisEnabled}
-          setInternalIsEnabled={setInternalIsEnabled}
-          setExternalIsEnabled={setExternalIsEnabled}
-        />
-        <PlasterDropdown
-          selectedPlaster={selectedPlaster}
-          setSelectedPlaster={setSelectedPlaster}
-          plasters={plasterData}
-        />
-        {errorMessage ? (
-          <Text style={{ color: "red" }}>{errorMessage}</Text>
-        ) : null}
-        <InputDisplayArea
-          lengthInput={lengthInput}
-          widthInput={widthInput}
-          thicknessInput={thicknessInput}
-          setLengthInput={setLengthInput}
-          setWidthInput={setWidthInput}
-          setThicknessInput={setThicknessInput}
-          contingencyInput={contingencyInput}
-          setContingencyInput={setContingencyInput}
-          calculateSum={handleCalculation}
-        />
-        <Text style={{ color: "slategrey", fontSize: 27 }}>RESULTS</Text>
+    <>
+      {/* <Button title="?" onPress={() => setModalVisible(true)} />
 
-        <OutputDisplayArea
-          label={"Total area (Square metres):"}
-          sum={outputResults.totalArea}
-          plasterNeeded={outputResults.plasterNeeded}
-          bagsNeeded={outputResults.bagsNeeded}
-          contingencyNeeded={outputResults.contingencyNeeded}
-          totalPlasterNeeded={outputResults.totalPlasterNeeded}
-        />
-        {selectedPlaster && (
-          <DataSheetButtons selectedPlaster={selectedPlaster} />
-        )}
+      <PlasterSearchModal
+        visible={modalVisible}
+        onClose={() => setModalVisible(false)}
+      /> */}
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        <View style={styles.container}>
+          <Text style={styles.title}>PLASTER CALCULATOR</Text>
+          <HorizontalRule />
 
-        <StatusBar style="auto" />
-      </View>
-    </ScrollView>
+          <PlasterTypeSwitch
+            InternalisEnabled={InternalisEnabled}
+            ExternalisEnabled={ExternalisEnabled}
+            setInternalIsEnabled={setInternalIsEnabled}
+            setExternalIsEnabled={setExternalIsEnabled}
+          />
+          <PlasterDropdown
+            selectedPlaster={selectedPlaster}
+            setSelectedPlaster={setSelectedPlaster}
+            plasters={plasterData}
+          />
+          {errorMessage ? (
+            <Text style={{ color: "red" }}>{errorMessage}</Text>
+          ) : null}
+          <InputDisplayArea
+            lengthInput={lengthInput}
+            widthInput={widthInput}
+            thicknessInput={thicknessInput}
+            setLengthInput={setLengthInput}
+            setWidthInput={setWidthInput}
+            setThicknessInput={setThicknessInput}
+            contingencyInput={contingencyInput}
+            setContingencyInput={setContingencyInput}
+            calculateSum={handleCalculation}
+          />
+          <Text style={{ color: "slategrey", fontSize: 27 }}>RESULTS</Text>
+
+          <OutputDisplayArea
+            label={"Total area (Square metres):"}
+            sum={outputResults.totalArea}
+            plasterNeeded={outputResults.plasterNeeded}
+            bagsNeeded={outputResults.bagsNeeded}
+            contingencyNeeded={outputResults.contingencyNeeded}
+            totalPlasterNeeded={outputResults.totalPlasterNeeded}
+          />
+          {selectedPlaster && (
+            <DataSheetButtons selectedPlaster={selectedPlaster} />
+          )}
+
+          <StatusBar style="auto" />
+        </View>
+      </ScrollView>
+    </>
   );
 };
 
@@ -94,7 +108,7 @@ const styles = StyleSheet.create({
     fontSize: 27,
     paddingTop: 10,
     marginBottom: 10,
-    color: "slategrey",
+    color: "red",
   },
   scrollViewContent: {
     flexGrow: 1,
